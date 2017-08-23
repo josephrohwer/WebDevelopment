d3d21a14ab815d5f119610e016804b645e5cb6dc
Joseph Rohwer <joseph.d.rohwer@gmail.com>
2017-08-22 23:32:08 -0500

    Created the model and the database

---
+++ BloggingApplication/src/main/java/com/josephrohwer/libraryapplication/dao/LibraryDao.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.josephrohwer.libraryapplication.dao;
+
+import com.josephrohwer.libraryapplication.dto.Author;
+import com.josephrohwer.libraryapplication.dto.Book;
+import com.josephrohwer.libraryapplication.dto.Publisher;
+import java.util.List;
+
+/**
+ *
+ * @author joseph.rohwer
+ */
+public interface LibraryDao {
+
+    public void addAuthor(Author author);
+
+    public void deleteAuthor(int authorId);
+
+    public void updateAuthor(Author author);
+
+    public Author getAuthorById(int id);
+
+    public List<Author> getAuthorsByBookId(int bookId);
+
+    public List<Author> getAllAuthors();
+
+    public void addBook(Book book);
+
+    public void deleteBook(int bookId);
+
+    public void updateBook(Book book);
+
+    public Book getBookById(int id);
+
+    public List<Book> getBooksByAuthorId(int authorId);
+
+    public List<Book> getBooksByPublisherId(int publisherId);
+
+    public List<Book> getAllBooks();
+
+    public void addPublisher(Publisher publisher);
+
+    public void deletePublisher(int publisherId);
+
+    public void updatePublisher(Publisher publisher);
+
+    public Publisher getPublisherById(int id);
+
+    public Publisher getPublisherByBookId(int bookId);
+
+    public List<Publisher> getAllPublishers();
+}
---
+++ BloggingApplication/src/main/java/com/josephrohwer/libraryapplication/main/LibraryApplication.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.josephrohwer.libraryapplication.main;
+
+/**
+ *
+ * @author joseph.rohwer
+ */
+public class LibraryApplication {
+    public static void main(String[] args) {
+        
+    }
+}
