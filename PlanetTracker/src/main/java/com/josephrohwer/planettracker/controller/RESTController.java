af0335cfb6c2a021652eba2df3c33b276f455e2e
Joseph Rohwer <joseph.d.rohwer@gmail.com>
2017-09-20 11:59:33 -0500

    Added REST endpoints

---
+++ PlanetTracker/src/main/java/com/josephrohwer/planettracker/controller/RESTController.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.josephrohwer.planettracker.controller;
+
+import com.josephrohwer.planettracker.dao.PlanetTrackerDao;
+import com.josephrohwer.planettracker.model.Planet;
+import java.util.List;
+import javax.inject.Inject;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ *
+ * @author joseph.rohwer
+ */
+@CrossOrigin
+@Controller
+public class RESTController {
+
+    private PlanetTrackerDao dao;
+
+    @Inject
+    public RESTController(PlanetTrackerDao dao) {
+        this.dao = dao;
+    }
+
+    @RequestMapping(value = "/planet/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Planet getPlanet(@PathVariable("id") long id) {
+        return dao.getPlanetById(id);
+    }
+
+    @RequestMapping(value = "/planet", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public Planet createPlanet(@RequestBody Planet planet) {
+        return dao.addPlanet(planet);
+    }
+
+    @RequestMapping(value = "/planet/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePlanet(@PathVariable("id") long id) {
+        dao.removePlanet(id);
+    }
+
+    @RequestMapping(value = "/planet/{id}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updatePlanet(@PathVariable("id") long id, @RequestBody Planet planet) {
+        planet.setPlanetId(id);
+        dao.updatePlanet(planet);
+    }
+
+    @RequestMapping(value = "/planets", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Planet> getAllPlanets() {
+        return dao.getAllPlanets();
+    }
+}
